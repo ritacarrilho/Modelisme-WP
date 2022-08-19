1526ae48d2f5dd5e15ca14d58995ce59102962ee
ritacarrilho <ritaclameira@gmail.com>
2022-08-19 22:31:57 +0200

    weather widget

--- wp-content/themes/occitanie/content-clubs.php
+++ wp-content/themes/occitanie/content-clubs.php
@@ -1,6 +1,6 @@
 <?php
 
-$clubs = $wpdb->get_results($wpdb->prepare("SELECT {$wpdb->prefix}clubs.id, {$wpdb->prefix}clubs.participant, {$wpdb->prefix}clubs.name, {$wpdb->prefix}categories.name as category
+$clubs = $wpdb->get_results($wpdb->prepare("SELECT {$wpdb->prefix}clubs.id, {$wpdb->prefix}clubs.participant, {$wpdb->prefix}clubs.name, {$wpdb->prefix}clubs.email, {$wpdb->prefix}categories.name as category
                                             FROM {$wpdb->prefix}clubs
                                             JOIN {$wpdb->prefix}categories
                                             ON {$wpdb->prefix}clubs.domain = {$wpdb->prefix}categories.id;"));
@@ -10,7 +10,8 @@ $members = $wpdb->get_results($wpdb->prepare("SELECT {$wpdb->prefix}clubs.id as 
                                                 JOIN {$wpdb->prefix}adherents 
                                                 ON {$wpdb->prefix}clubs.id = {$wpdb->prefix}adherents.club_id;"));
 
-// $count_members = $wpdb->get_results("SELECT COUNT({$wpdb->prefix}adherents.id) FROM {$wpdb->prefix}adherents;");
+$count_members = $wpdb->get_results("SELECT COUNT({$wpdb->prefix}adherents.id) FROM {$wpdb->prefix}adherents WHERE {$wpdb->prefix}adherents.club_id=$id;");
+
 
 // array with clubs ids from table members
 $clubs_with_members_id = array_column($members, 'club');
@@ -20,7 +21,7 @@ $all_clubs_id = array_column($clubs, 'id');
 $res = array_intersect($all_clubs_id, $clubs_with_members_id);
 
 //  var_dump($count_members);
-// echo '<pre>'; print_r($members); echo '</pre>';
+echo '<pre>'; print_r($count_members); echo '</pre>';
 ?>
 
 <main class="clubs">
@@ -31,22 +32,25 @@ $res = array_intersect($all_clubs_id, $clubs_with_members_id);
             <?php 
             if(isset($clubs)) :
                 foreach($clubs as $club) :?>
-                    <?php if($club->participant == 1) { ?>
-                        <div>
-                            <h4><?php echo $club->name?></h4> 
+                    <div>
+                        <?php if($club->participant == 1) { ?>
+                            <h4 class="h4-padding-top"><?php echo $club->name?></h4> 
+                            <h6 class="h6-category">Email: <span> <?php echo $club->email ?> </span></h6>
                             <h6 class="h6-category">Catégorie: <span> <?php echo $club->category ?> </span></h6>
-                                <?php foreach($members as $member) {
-                                    if(isset($member->club) && $club->id == $member->club) { ?>
-                                        <p> Nom: <?php echo "{$member->first_name} {$member->last_name}"?> </p>
-                                        <small> Nombre d'adhérent:  <?php echo $member->club_number ?> </small>
+                            <h6 class="p-padding-bottom"> Adhérents:</h6>
+                            
+                                <?php foreach($members as $member) { ?>
+                                    <?php if(isset($member->club) && $club->id == $member->club) { 
+                                        $id = $club->id; ?>
+                                        <p class="p-padding-bottom"><?php echo $count_members ?> </p>
                                 <?php } 
                                 }
                             
                                 if(!in_array($club->id, $res)) { ?> 
-                                    <p> Ce Club n'a pas encore de adhérents</p>
+                                    <p id="p-padding-bottom"> Ce Club n'a pas encore de adhérents</p>
                             <?php }
                         } ?>
-                        </div>
+                    </div>
             <?php endforeach; 
             endif; ?>         
         </div>
--- wp-content/themes/occitanie/functions.php
+++ wp-content/themes/occitanie/functions.php
@@ -88,3 +88,168 @@ function footer() {
 add_action( 'wp_footer', 'footer' ); // Hook the widget initiation and run our function
 
 // TODO: metadata and posts change in front office
+
+function modelisme_widget()
+{
+    register_sidebar(array(
+        "name" => "modelisme_widget",
+        "description" => __("Les widgets de l'ern", 'ern2021'),
+        "id" => "modelismewidget",
+        "before_widget" => "<div class='border border-info mb-3 rounded p-2'>",
+        "after_widget" => "</div>",
+        "before_title" => "<h4 class='widget-title'>",
+        "after_title" => "</h4>"
+    ));
+
+    register_widget('WP_Widget_Weather');
+}
+
+add_action("widgets_init", "modelisme_widget");
+
+
+class WP_Widget_Weather extends WP_Widget
+{
+    function __construct() 
+    {
+        $widget_ops = array (
+            'classname' => 'widget_weather',
+            'description' => __('Dark Ski Api meteo'),
+            'customize_selective_refresh' => true,
+        );
+        parent::__construct('weather', __('Weather Widget', 'Weather', $widget_ops));
+    }
+
+    public function form($instance) 
+    {
+        $instance = wp_parse_args( (array)$instance , array(
+            "lat" => '',
+            "lon" => '',
+            "apiToken" => '',
+         ));
+         ?>
+
+         <p>
+            <label for="<?php echo $this->get_field_id('lat');?>" <?php _e('Lattitude :') ?>></label>
+            <input type="text" class="" id="<?php echo $this->get_field_id('lat'); ?>" name="<?php echo $this->get_field_name('lat'); ?>" value="<?php echo esc_attr( $instance['lat']); ?>">
+         </p>
+         <p>
+            <label for="<?php echo $this->get_field_id('lon');?>" <?php _e('Longitude :') ?>></label>
+            <input type="text" class="" id="<?php echo $this->get_field_id('lon'); ?>" name="<?php echo $this->get_field_name('lon'); ?>" value="<?php echo esc_attr( $instance['lon']); ?>">
+         </p>
+         <p>
+            <label for="<?php echo $this->get_field_id('apiToken');?>" <?php _e('apiToken :') ?>></label>
+            <input type="text" class="" id="<?php echo $this->get_field_id('apiToken'); ?>" name="<?php echo $this->get_field_name('apiToken'); ?>" value="<?php echo esc_attr( $instance['apiToken']); ?>">
+         </p>
+    <?php 
+    }
+
+    public function update($new_instance, $old_instance) 
+    {
+        $instance = $old_instance;
+        $instance['lat'] = sanitize_text_field( $new_instance['lat']);
+        $instance['lon'] = sanitize_text_field( $new_instance['lon']);
+        $instance['apiToken'] = sanitize_text_field( $new_instance['apiToken']);
+
+        return $new_instance;
+    }
+
+    // show in front-end
+    public function widget($args, $instance)
+    {
+        // title
+        $title = 'La Méteo';
+        // api request url
+        $url = 'https://api.darksky.net/forecast' . $instance['apiToken'] . '/' . $instance['lat'] . ',' . $instance['lon'];
+
+        // api request
+        $request = wp_remote_get($url);
+
+        if(is_wp_error( $request )) {
+            return false;
+        }
+
+        $body = wp_remote_retrieve_body($request);
+        $data = json_decode($body);
+        echo $args['before_widget'];
+
+        if($title) {
+            echo $args['before-title'] . $title . $args['after-title'];
+        }
+
+        echo '<div id="meteo-wrap" class="meteo-wrap">';
+        if(!empty($data)) {
+            echo '<div class="'.$data->currently->icon.'">&nbsp;</div>';
+            echo'<div>' . number_format((( $data->currently->temperature - 32) * (5/9)), 2, ',', ' ') . 'deg</div>';
+            echo'<div>' . number_format(( $data->currently->windSpeed * 1.609), 2, ',', ' ') . 'km</div>';
+        }
+        
+        echo'</div>';
+        echo $args['after-widget'];
+    }
+}
+
+
+// Creating the widget
+class wpb_widget extends WP_Widget {
+ 
+    function __construct() {
+    parent::__construct(
+     
+    // Base ID of your widget
+    'wpb_widget', 
+     
+    // Widget name will appear in UI
+    __('WPBeginner Widget', 'wpb_widget_domain'), 
+     
+    // Widget description
+    array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), )
+    );
+    }
+     
+    // Creating widget front-end
+     
+    public function widget( $args, $instance ) {
+    $title = apply_filters( 'widget_title', $instance['title'] );
+     
+    // before and after widget arguments are defined by themes
+    echo $args['before_widget'];
+    if ( ! empty( $title ) )
+    echo $args['before_title'] . $title . $args['after_title'];
+     
+    // This is where you run the code and display the output
+    echo __( 'Hello, World!', 'wpb_widget_domain' );
+    echo $args['after_widget'];
+    }
+     
+    // Widget Backend
+    public function form( $instance ) {
+    if ( isset( $instance[ 'title' ] ) ) {
+    $title = $instance[ 'title' ];
+    }
+    else {
+    $title = __( 'New title', 'wpb_widget_domain' );
+    }
+    // Widget admin form
+    ?>
+    <p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+    <?php
+    }
+     
+    // Updating widget replacing old instances with new
+    public function update( $new_instance, $old_instance ) {
+    $instance = array();
+    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+    return $instance;
+    }
+     
+    // Class wpb_widget ends here
+    } 
+
+    // Register and load the widget
+function wpb_load_widget() {
+    register_widget( 'wpb_widget' );
+}
+add_action( 'widgets_init', 'wpb_load_widget' );
