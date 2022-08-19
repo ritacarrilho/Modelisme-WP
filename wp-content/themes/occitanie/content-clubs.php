ce565ad253d7e95bc56f62a92e5338aa439893e6
ritacarrilho <ritaclameira@gmail.com>
2022-08-19 18:10:26 +0200

    created algorithm to display the name of each club's members

--- wp-content/themes/occitanie/content-clubs.php
+++ wp-content/themes/occitanie/content-clubs.php
@@ -1,57 +1,75 @@
 <?php
 
 $clubs = $wpdb->get_results($wpdb->prepare("SELECT * FROM {$wpdb->prefix}clubs;"));
-$members = $wpdb->get_results($wpdb->prepare("SELECT * FROM {$wpdb->prefix}adherents;"));
+$members = $wpdb->get_results($wpdb->prepare("SELECT {$wpdb->prefix}clubs.id as club, {$wpdb->prefix}adherents.id as member, {$wpdb->prefix}adherents.first_name, {$wpdb->prefix}adherents.last_name
+                                                FROM {$wpdb->prefix}clubs
+                                                JOIN {$wpdb->prefix}adherents 
+                                                ON {$wpdb->prefix}clubs.id = {$wpdb->prefix}adherents.club_id;"));
 
+// array with ids from clubs with members
+$clubs_with_members_id = array_column($members, 'club');
+$all_clubs_id = array_column($clubs, 'id');
 
-echo '<pre>'; print_r($clubs); echo '</pre>';
-echo '<pre>'; print_r($members); echo '</pre>';
+$res = array_intersect($all_clubs_id, $clubs_with_members_id);
+var_dump($res);
+
+// var_dump($object_id);
+echo '<pre>'; print_r($clubs_with_members_id); echo '</pre>';
+echo '<pre>'; print_r($all_clubs_id); echo '</pre>';
+
+// echo '<pre>'; print_r($clubs); echo '</pre>';
+// echo '<pre>'; print_r($members); echo '</pre>';
 
 ?>
 <section>
     <h2>Clubs participants</h2>
 
     <div>
-        <?php foreach($clubs as $club):
-            if($club->participant == 1) { ?>
-                <div>
-                <?php foreach($members as $member) {
-                    if($club->id === $member->club_id) { ?>
-                        <h5> <?php echo $club->name?> </h5>
-                        <p> <?php echo $member->first_name; echo $member->last_name?> </p>
-                <?php } else { ?>
-                    <h5> <?php echo $club->name?> </h5>
-                    <p> Ce Club n'a pas encore de adhérents</p>
-                <?php } 
-                } ?>
-                </div>
-        <?php } 
-                
-         endforeach; ?>         
+        <?php 
+        if(isset($clubs)) :
+            foreach($clubs as $club) :?>
+                <?php if($club->participant == 1) { ?>
+                    <div>
+                        <h5><?php echo $club->name?></h5> 
+                            <?php foreach($members as $member) {
+                                if(isset($member->club) && $club->id == $member->club) { ?>
+                                    <p> <?php echo "{$member->first_name} {$member->last_name}"?> </p>
+                            <?php } 
+                            }
+                        
+                            if(!in_array($club->id, $res)) { ?> 
+                                <p> Ce Club n'a pas encore de adhérents</p>
+                        <?php }
+                    } ?>
+                    </div>
+        <?php endforeach; 
+        endif; ?>         
     </div>
 </section>
 
+
 <section>
     <h2>Clubs non participants</h2>
-    <div>
-        <?php foreach($clubs as $club):
-            if($club->participant == 0) { ?>
-                 <div>
-           <?php foreach($members as $member) {
-                 if($club->id === $member->club_id) { ?>
-                    <h5> <?php echo $club->name?> </h5>
-                    <p> <?php echo $member->first_name; echo $member->last_name?> </p>
-            <?php } else { ?>
-                    <h5> <?php echo $club->name?> </h5>
-                    <p> Ce Club n'a pas encore de adhérents</p>
-                <?php } 
-                } ?>
-                 </div> 
-            <?php } 
-                
-        endforeach; ?>
-            
 
-             
+    <div>
+        <?php 
+        if(isset($clubs)) :
+            foreach($clubs as $club) :?>
+                <?php if($club->participant == 0) { ?>
+                    <div>
+                        <h5><?php echo $club->name?></h5> 
+                    <?php foreach($members as $member) {
+                                if(isset($member->club) && $club->id == $member->club) { ?>
+                                    <p> <?php echo "{$member->first_name} {$member->last_name}"?> </p>
+                            <?php } 
+                            }
+
+                            if(!in_array($club->id, $res)) { ?> 
+                                <p> Ce Club n'a pas encore de adhérents</p>
+                        <?php }
+                    } ?>
+                    </div>
+        <?php endforeach; 
+        endif; ?>         
     </div>
 </section>
